87bb41f495df0af04106e4abb9d58c2d14897145
yixingchin <57394787+yixingchin@users.noreply.github.com>
2021-11-24 14:57:50 +0800

    DH library

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ gojek = DiffieHellman(18)
 grab.generate_public_key()    # automatically generates private key
 gojek.generate_public_key()
 
+grab.generate_shared_secret(gojek.public_key, echo_return_key=True)
+gojek.generate_shared_secret(grab.public_key, echo_return_key=True)
 
 print(len(str(grab.public_key)))
+if grab.shared_secret == gojek.shared_secret:
+    print('true')
